7f6bd5d8bdb75f7ee601c1c0f36a497833fc0b77
Parth-Paliwal <221b261@juetguna.in>
2025-05-10 12:05:23 +0530

    authentication completed

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,27 +1,46 @@
-'use client'
+"use client";
 
 import { Video } from "lucide-react";
 import Container from "./Container";
 import { useRouter } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import { useAuth } from "@clerk/clerk-react";
+import { Button } from "../ui/button";
 
-const Navbar =({children} : {children? : React.ReactNode})=>{
-    const router = useRouter();
-return (
+const Navbar = ({ children }: { children?: React.ReactNode }) => {
+  const router = useRouter();
+  const { userId } = useAuth();
 
-
+  return (
     <div className="sticky top-0 border border-b-primary/10">
-        <Container>
-            <div>
-                <div className="flex items-center gap-1 cursor-pointer" onClick={()=>{router.push("/")}}>
-                    <Video/>
-                    <div className="font-bold text-xl ">
-                        Peer Call
-                    </div>
-                </div>
-            </div>
-        </Container>
+      <Container>
+        <div className="flex items-center justify-between">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            <Video />
+            <div className="font-bold text-xl">Peer Call</div>
+          </div>
+
+          
+          <div className="flex gap-3 items-center">
+            <UserButton />
+            {!userId && (
+              <>
+                <Button size={"sm"} onClick={()=>{router.push("/sign-up")}} className="cursor-pointer">Sign Up</Button>
+                <Button size={"sm"} variant={"outline"} onClick={()=>{router.push("/sign-in")}} className="cursor-pointer">
+                  Sign In
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
     </div>
-)
-}
+  );
+};
 
 export default Navbar;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)' , '/']);
 
 
 export default clerkMiddleware(async (auth, req) => {
@@ -16,4 +16,5 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
+  publicRoutes: ["/"],
 }
